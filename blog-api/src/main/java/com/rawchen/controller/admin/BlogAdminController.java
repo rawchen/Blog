79edf710706b229db8c48588116a9c733db3036e
RawChen <2221999792@qq.com>
2021-07-03 23:50:53 +0800

    首页文章列表首图默认随机

--- blog-api/src/main/java/com/rawchen/controller/admin/BlogAdminController.java
+++ blog-api/src/main/java/com/rawchen/controller/admin/BlogAdminController.java
@@ -189,7 +189,7 @@ public class BlogAdminController {
 	 */
 	private Result getResult(com.rawchen.model.dto.Blog blog, String type) {
 		//验证普通字段
-		if (StringUtils.isEmpty(blog.getTitle(),blog.getFirstPicture(), blog.getContent(), blog.getDescription())) {
+		if (StringUtils.isEmpty(blog.getTitle(), blog.getContent(), blog.getDescription())) {
 			return Result.error("参数有误");
 		}
 
